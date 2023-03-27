4acb12f49e0ee65cd5ab232ff35dcb5bc68c7c5e
Aaron Macaraeg <whoseyouraaron@yahoo.com>
2023-03-26 22:23:39 -0700

    added menu

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+
+
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome." });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
---
+++ src/components/data.js
@@ -0,0 +1,27 @@
+const menu = [
+    {
+      id: 1,
+      title: "Banh Canh Cua",
+      category: "Soup",
+      price: 12.50,
+      img: "./images/banhcanh.png",
+      desc: `Crab noodle soup`,
+    },
+    {
+      id: 2,
+      title: "Bun Bo Hue",
+      category: "Soup",
+      price: 12.50,
+      img: "./images/bunbohue.png",
+      desc: `Spicy beef noodle soup`,
+    },
+    {
+      id: 3,
+      title: "Beef Pho",
+      category: "Soup",
+      price: 12.50,
+      img: "./images/beefpho.png",
+      desc: `Beef broth noodle soup`,
+    },
+  ];
+  export default menu;
